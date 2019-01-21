02ed58ffc3b95ec39c409ed544ff02922a4bae93
Panumat Makaew <panumas.makaew@gmail.com>
2019-01-21 16:54:41 +0700

    add create method and bra bra so many

--- app/model.js
+++ app/model.js
@@ -5,15 +5,25 @@ import db from '../db'
 const Model = {
     findAll(){
                     // key ประกาศ ใน app->users->model
-        return db[this.key]
+        // return db[this.key]
+        return this.collection()
     },
 
     find(id){
-
+        // return db[this.key].find(user => user.id === id)
+        return this.findRecord(id)
     },
 
     create(attrs){
+        const collection = this.collection()
+        const record = 
+                                            //สร้าง id ให้มัน
+            this.withPermitedAttrs(attrs, { id: collection.length +1 })
+                        //ค่า record เดิม รวมกับ record ใหม่ ที่พึ่งสร้าง -> เอามาใส่ใน array
+        this.setCollection([...collection,record])
 
+        //คืนผลลัพทืที่พึ่งสร้างเสร็จ
+        return record 
     },
 
     update(id,attrs){
@@ -22,6 +32,32 @@ const Model = {
 
     destroy(id){
 
+    },
+
+    //ช่วยเรื่องการซ้ำซ้อน
+    collection(){
+        return db[this.key]
+    },
+
+    findRecord(id){
+                                                            // + แปลงค่าให้ตรง
+        return this.collection().find(record => record.id === +id)
+    },
+                            //ค่าเริ่มต้น ไม่ส่งมาค่าก็ว่าง
+    withPermitedAttrs(attrs,init = {} ){
+                    //มาจาก app->user->model.js
+        return this.permittedAttrs.reduce(
+            // reduce รับ record กับ attr
+            (record,attr) => 
+                //attrs ที่เรารับเข้ามามี permittedAttrs อยู่ไหม
+                                //มี คืน record กับ ค่า          // ไม่มี คือ record
+                attrs[attr] ? {...record,[attr]:attrs[attr]} : record
+            ,init )
+    },
+    
+    // เปลี่ยน collection ให้เป็นปัจจุบัน
+    setCollection(collection){
+        db[this.key] = collection
     }
 }
 
--- app/users/model.js
+++ app/users/model.js
@@ -2,12 +2,14 @@
 
 import db from '../../db'
 import Model from '../model';
+import { promises } from 'fs';
 
 const Users = {
     // ... อะไรก็ตามที่ประกาศภายใต้ model ให้บรรจุใน user ด้วย
     ...Model,
     //ให้รู้ว่าจะค้นอะไร
-    key: 'users'
+    key: 'users',
+    permittedAttrs: ['email']
 
     // ใช้ findAll ใน Model แทน
     // findAll(){
