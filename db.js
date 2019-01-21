33a4a74ea58cd48ac0b45501e3735d0af2e0f06f
Panumat Makaew <panumas.makaew@gmail.com>
2019-01-21 14:59:14 +0700

    add database in file db.js

---
+++ db.js
@@ -0,0 +1,43 @@
+//จำลอง database
+export default {
+    "articles": [
+      {
+        id: 1,
+        title: "Introduction to JavaScript",
+        content : 'Lorem Ipsum',
+        authorId: 1
+      },
+      {
+        id: 2,
+        title: "Introduction to Python",
+        content : 'Lorem Ipsum',
+        authorId: 1
+      },
+      {
+        id: 3,
+        title: "Introduction to Elixir",
+        content : 'Lorem Ipsum',
+        authorId: 2
+      },
+      {
+        id: 4,
+        title: "Introduction to Ruby",
+        content : 'Lorem Ipsum',
+        authorId: 2
+      },
+    ],
+    user: [
+      {
+        id: 1,
+        email : 'babelcoder@gmail.com',
+        isAdmin: true ,
+        password : '------'
+      },
+      {
+        id: 2,
+        email : 'somchai@gmail.com',
+        isAdmin: true ,
+        password : '------'
+      },
+    ]
+  }
